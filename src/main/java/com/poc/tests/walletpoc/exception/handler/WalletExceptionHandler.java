ae43fd7a5d3a48dfc24f7e0164812e59eb6f3ccd
Someone <milomir.radojevic93@gmail.com.com>
2021-08-10 18:03:35 +0200

    Add exception handling.

---
+++ src/main/java/com/poc/tests/walletpoc/exception/handler/WalletExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.poc.tests.walletpoc.exception.handler;
+
+import com.poc.tests.walletpoc.dto.ErrorResponse;
+import com.poc.tests.walletpoc.exception.InsufficientFundsException;
+import com.poc.tests.walletpoc.exception.NotFoundException;
+import com.poc.tests.walletpoc.exception.StripeServiceException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class WalletExceptionHandler {
+    @ExceptionHandler(value = {NotFoundException.class, InsufficientFundsException.class, StripeServiceException.class})
+    public ResponseEntity<Object> handleException(Exception e) {
+        return new ResponseEntity<>(new ErrorResponse(e.getClass().getSimpleName() + " was thrown"), HttpStatus.CONFLICT);
+    }
+}
